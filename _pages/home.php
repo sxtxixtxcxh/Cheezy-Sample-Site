e94d322aa0a05a36622bb4576577912eb347891e
M. Auayan <sxtxixtxcxh@gmail.com>
2010-03-06 22:12:48 -0800

    added cheezy as a submodule

---
+++ _pages/home.php
@@ -0,0 +1,12 @@
+<h1>Home Page</h1>
+
+<?php
+// require_once APP_ROOT.'/lib/db.php';
+// 
+// $user = User::find_by_screenname("sxtxixtxcxh");
+// echo '<pre style="clear:left;text-align:left">';
+// var_dump($user);
+// echo '</pre>';
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php 
+  require_once '../vendor/cheezy/dispatch.php';
+  Dispatcher::dispatch();
+?>
+
