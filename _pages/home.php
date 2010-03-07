f2a69cf6a5546085be22d893c3797cec87e76480
M. Auayan <sxtxixtxcxh@gmail.com>
2010-03-07 01:43:38 -0800

    added "Who's using it" section.

--- _pages/home.php
+++ _pages/home.php
@@ -8,6 +8,8 @@
   <dd>You know how there are all sorts of "lightweight" frameworks but they don't ever really seem to be so lightweight or simple? Well, that's what I'm hoping this fixes.</dd>
   <dt>What does this require?</dt>
   <dd>Not much: PHP 5, Apache. If you need a web host, I currently use DreamHost. Sign up <a href="http://www.dreamhost.com/r.cgi?146412">using this link</a> and they'll credit me for the referral.</dd>
+  <dt>Who's using it?</dt>
+  <dd>This site, for one. And my friend <a href="http://davidhoang.com">David Hoang</a>. <a href="/contact">Let me know</a>, if you're using it!</dd>
   <dt>Where can I download this?</dt>
   <dd>You can download the source of this sample site and the framework from <a href="http://github.com/sxtxixtxcxh/Cheezy-Sample-Site">github</a>.</dd>
 </dl>
