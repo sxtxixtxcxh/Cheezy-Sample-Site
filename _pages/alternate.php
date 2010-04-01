e6143794344b5c18eeb7255c949f2f2ebf1f1f52
M. Auayan <sxtxixtxcxh@gmail.com>
2010-03-31 23:19:01 -0700

    added content to alternate, updated tag in subpage

--- _pages/alternate.php
+++ _pages/alternate.php
@@ -1,2 +1,5 @@
 <?php $this->layout = 'alternate'; ?>
 <h1>WOW that's cheezy</h1>
+<p>This page is has a special command that tells it to use the 'alternate' layout inside the <code>_layouts</code> folder</p>
+  
+<p>All you have to do is create the layout and tell the page to switch to that layout!</p>
--- _pages/subdir/subpage.php
+++ _pages/subdir/subpage.php
@@ -1,6 +1,6 @@
 <?php $this->title = 'A Subdirectory!'?>
 <h1>¡ɹǝpun uʍop ɯoɹɟ ¡ıɥ 'ɥo</h1>
 
-<p>This page is under a subdirectory... You can find it in _pages/subdir/subpage.php</p>
+<p>This page is under a subdirectory... You can find it in <code>_pages/subdir/subpage.php</code></p>
   
 <p>It's as simple as you might think: just make a subdirectory inside the _pages directory, and start putting pages there!</p>
